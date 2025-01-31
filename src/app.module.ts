055b4b5e189fe6928b01bc8e9a7c254c8cb9ee88
Tanay-Jain15 <tanaydalawat@gmail.com>
2025-01-31 13:34:32 +0530

    initial commit. added slack resource repo, env file and readme

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SlackModule } from './slack/slack.module';
+import { ConfigModule } from '@nestjs/config';
+
+
+@Module({
+  imports: [SlackModule,  ConfigModule.forRoot({
+    isGlobal: true, // It makes the config globally available
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
