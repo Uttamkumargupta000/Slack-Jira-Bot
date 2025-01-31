1b2d8cabf7d715b14ebf8d90ee01237251334cea
Tanay-Jain15 <tanaydalawat@gmail.com>
2025-01-31 17:54:52 +0530

    added code for slack-bot to connect with server, in slack.controller.ts.

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Query } from '@nestjs/common';
 import { SlackService } from './slack.service';
+import { query } from 'express';
 
 @Controller('slack')
 export class SlackController {
   constructor(private readonly slackService: SlackService) {}
+
+  @Post('events')
+  async handleSlackEvent(@Body() body: any, @Query ('challenge') challenge:string) {
+    console.log('sent the webhook url')
+
+    // Slack URL verification 
+    
+    if(challenge){
+      return {challenge}
+    }
+
+    // Handling message events
+    if (body.event?.type === 'message') {
+      return this.slackService.respondToMessage(body.event);
+    }
+
+    //This helps your webhook stay active and responsive, even if it doesn’t need to take any action.
+    return { status:"ok received." };
+  }
 }
