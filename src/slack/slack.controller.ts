1edf7e3bf5b9685524fcd930f9baca45674df11b
Tanay-Jain15 <tanaydalawat@gmail.com>
2025-02-04 16:09:43 +0530

    modified slack.controller.ts

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { SlackService } from './slack.service';
 import { SlackEventDto } from './dto/slack-event.dto';
-// import { SlackEventDataDto } from './dto/slack-event.dto'; 
-
 
 @Controller('slack')
 export class SlackController {  
