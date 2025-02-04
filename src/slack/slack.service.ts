8bc43c020562c524de583771efe8f01a91897948
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-04 11:43:39 +0530

    added function to send message

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { WebClient } from '@slack/web-api';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class SlackService {}
+export class SlackService {
+  private slackClient: WebClient;
+
+  constructor(private configService: ConfigService) {
+    const token = this.configService.get<string>('SLACK_BOT_TOKEN')
+    this.slackClient = new WebClient(token);
+  }
+
+
+  async sendMessage(channel: string, text: string): Promise<any> {
+    try {
+      const response = await this.slackClient.chat.postMessage({
+        channel,
+        text,
+      });
+      console.log('Message sent successfully');
+      return response.message;
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  }
+
+  async fetchMessages(channel: string): Promise<any> {
+    try {
+      const response = await this.slackClient.conversations.history({ channel });
+      console.log(response);
+      return response.messages;
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+    }
+  }
+}
