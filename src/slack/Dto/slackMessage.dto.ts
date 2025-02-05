57bb56fefae99af19e214c9a6b4fc633089e8b3b
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-05 10:43:30 +0530

    final dto class based

---
+++ src/slack/Dto/slackMessage.dto.ts
@@ -0,0 +1,52 @@
+import { Type } from "class-transformer";
+import { IsOptional, IsString } from "class-validator";
+
+ export class SlackEventDataTypeDto{
+  @IsOptional()
+  @IsString()
+  user: string;
+
+  @IsOptional()
+  @IsString()
+  text: string;
+
+  @IsOptional()
+  @IsString()
+  type: string;
+  
+  @IsOptional()
+  @IsString()
+  channel: string;
+
+  @IsOptional()
+  @IsString()
+  channel_types: string;
+
+  @IsOptional()
+  @IsString()
+  bot_id: string;
+
+  @IsOptional()
+  @IsString()
+  subtype: string;
+
+  @IsOptional()
+  @IsString()
+  status: string;
+ }
+
+ export class SlackEventDto{
+  @IsOptional()
+  @IsString()
+  challenge?: string;
+
+  @IsOptional()
+  @IsString()
+  type?: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => SlackEventDataTypeDto)
+  event?: SlackEventDataTypeDto
+ }
+ 
