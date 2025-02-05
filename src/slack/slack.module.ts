d47fe43167f93ac0acbed67c3b59ccc3e6afcf64
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-05 10:39:03 +0530

    removed any keywords

--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SlackService } from './slack.service';
 import { SlackController } from './slack.controller';
 
 @Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [SlackController],
   providers: [SlackService],
 })
