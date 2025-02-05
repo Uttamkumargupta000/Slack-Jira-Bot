6302a9c62c073e17f53bd2eff96566ec0822fe19
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-05 10:38:04 +0530

    removed any Keywords

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,37 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { WebClient } from '@slack/web-api';
 import { ConfigService } from '@nestjs/config';
+import { SlackEventDto } from './Dto/slackMessage.dto';
 
 @Injectable()
 export class SlackService {
   private slackClient: WebClient;
 
   constructor(private configService: ConfigService) {
-    const token = this.configService.get<string>('SLACK_BOT_TOKEN')
+    const token = this.configService.get<string>('SLACK_BOT_TOKEN');
     this.slackClient = new WebClient(token);
   }
 
-
-  async sendMessage(channel: string, text: string): Promise<any> {
+  async sendMessage(channel: string, text: string) {
     try {
       const response = await this.slackClient.chat.postMessage({
         channel,
         text,
       });
-      console.log('Message sent successfully');
-      return response.message;
+      console.log('Message sent successfully:', response);
+      return response as SlackEventDto;
     } catch (error) {
       console.error('Error sending message:', error);
-    }
-  }
-
-  async fetchMessages(channel: string): Promise<any> {
-    try {
-      const response = await this.slackClient.conversations.history({ channel });
-      console.log(response);
-      return response.messages;
-    } catch (error) {
-      console.error('Error fetching messages:', error);
     }
   }
 }
