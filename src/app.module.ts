282cf604068222dee745119f8b9a4e691bd04559
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-05 10:40:41 +0530

    modified changes with importers and PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { SlackModule } from './slack/slack.module';
 import { ConfigModule } from '@nestjs/config';
 
-
 @Module({
-  imports: [SlackModule,  ConfigModule.forRoot({
-    isGlobal: true, // It makes the config globally available
-  }),],
+  imports: [
+    SlackModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // It makes the config globally available
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
