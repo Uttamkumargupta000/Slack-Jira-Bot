6a3f7aee358187f1c19159eec4df10c5914acc8a
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-06 11:33:29 +0530

    added pipe validation of object

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: false,
+      forbidNonWhitelisted: false,
+    }),
+  );
+
+  // DLFSDIHF
+  // LFKJBSDIFH
+  // FLKJKDS
+
+
+  // DHFLKJKDSDFFLKKH
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
