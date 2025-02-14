e4a03ff7c7e0b42a459281ad1016561b7c212647
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-14 16:30:13 +0530

    error resolved in importing dto file

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { WebClient } from '@slack/web-api';
 import { ConfigService } from '@nestjs/config';
-import { SlackEventDto } from './dto/slack.event.dto';
+import { SlackEventDto } from './Dto/slack.event.dto';
 
 
 @Injectable()
