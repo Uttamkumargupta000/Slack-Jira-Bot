cffe8d7d91102ed67ad2f344e86c9c8f7f83a6ea
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-19 00:25:23 +0530

    added dto to handle slackevents

--- src/slack/Dto/slack.event.dto.ts
+++ src/slack/Dto/slack.event.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsOptional, IsString } from "class-validator";
+import { IsOptional, IsString, ValidateNested } from "class-validator";
 
  export class SlackEventDataTypeDto{
   @IsOptional()
@@ -20,7 +20,7 @@ import { IsOptional, IsString } from "class-validator";
 
   @IsOptional()
   @IsString()
-  channel_type: string;
+  channel_types: string;
 
   @IsOptional()
   @IsString()
@@ -33,9 +33,9 @@ import { IsOptional, IsString } from "class-validator";
   @IsOptional()
   @IsString()
   status: string;
-}
+ }
 
-export class SlackEventDto {
+ export class SlackEventDto{
   @IsOptional()
   @IsString()
   challenge?: string;
@@ -45,7 +45,7 @@ export class SlackEventDto {
   type?: string;
 
   @IsOptional()
-  @IsString()
+  @ValidateNested()
   @Type(() => SlackEventDataTypeDto)
   event?: SlackEventDataTypeDto
  }
