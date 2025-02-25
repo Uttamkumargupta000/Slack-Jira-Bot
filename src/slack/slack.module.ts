8cadafcf0e982e57435b8cc83bfdf36466d126e3
Uttamkumargupta000 <uttamkumargupta000@gmail.com>
2025-02-25 16:31:34 +0530

    added module for the messageCache

--- src/slack/slack.module.ts
+++ src/slack/slack.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { SlackService } from './slack.service';
 import { SlackController } from './slack.controller';
+import { HttpModule } from '@nestjs/axios';
+import { MessageCacheService } from './messageCacheService';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),HttpModule],
   controllers: [SlackController],
-  providers: [SlackService],
+  providers: [SlackService,MessageCacheService],
+  exports: [MessageCacheService]
 })
 export class SlackModule {}
